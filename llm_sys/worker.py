db5df06e4132a20d2e9b44a0ef9bb56a1922ae8b
Onlytonight <1198076162@qq.com>
2025-10-22 16:40:57 +0800

    默认提交信息：2025-10-22 16:40:57

--- llm_sys/worker.py
+++ llm_sys/worker.py
@@ -23,7 +23,8 @@ def init_engine(layer_ids, model_name, vram_usage=0.8):
     engine_args = EngineArgs(model=model_name, block_size=16,
                              load_format="dummy", enforce_eager=True,
                              swap_space=32, max_num_batched_tokens=4096,
-                             gpu_memory_utilization=vram_usage)
+                             gpu_memory_utilization=vram_usage,
+                             tensor_parallel_size=2)
 
     engine = PipelineStageEngine.from_engine_args(engine_args, layer_ids)
     return engine
